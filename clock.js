b0ce6a6a3ab4900d75fe8951edddd4c87c319e6e
emely rosa <emely.rosa@student.uncommonhs.org>
2016-08-08 10:57:31 -0400

    added numbers to the clock

--- clock.js
+++ clock.js
@@ -12,24 +12,50 @@ drawClock();
 
 function drawClock() {
     drawFace(ctx, radius);
+    drawNumbers(ctx, radius);
 }
 
 function drawFace(ctx, radius) {
     var grad;
-    ctx.beginPath();
+    ctx.beginPath(); /*draws the white circle */
     ctx.arc(0, 0, radius, 0, 2*Math.PI);
     ctx.fillStyle = 'white';
     ctx.fill();
+    /*creates a radial gradiant*/
     grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
+    /* color stops create the 3D effect*/
     grad.addColorStop(0, '#333');
     grad.addColorStop(0.5, 'white');
     grad.addColorStop(1, '#333');
-    ctx.strokeStyle = grad;
+    ctx.strokeStyle = grad; /*defines the gradiant */
     ctx.lineWidth = radius*0.1;
-    ctx.stroke();
-    ctx.beginPath();
+    ctx.stroke();/*draws the cicle */
+    ctx.beginPath();/*draws the black dot in the center*/
     ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
     ctx.fillStyle = '#333';
     ctx.fill();
 }
 /*end of clock face */
+
+/*adding numbers to the clock*/
+
+function drawNumbers(ctx, radius) {
+    var ang;
+    var num;
+    ctx.font = radius*0.15 + "px arial"; /*font size*/
+    ctx.textBaseline="middle"; /*text alignment*/
+    ctx.textAlign="center";
+    /*print position of each number 1-12 (85% of the radius)*/
+    for(num= 1; num < 13; num++){
+        ang = num * Math.PI / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius*0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius*0.85);
+        ctx.rotate(-ang);
+    }
+}
+/*numbers*/
+
